fd0d296509b9e869875dbe31d3689be08f8cf439
kingjeremyng <jeremy97ng@gmail.com>
2025-02-17 13:36:21 -0500

    added youtube filter

---
+++ src/filterHome.js
@@ -0,0 +1,42 @@
+function splitAtIndex(value, index) {
+    return [Number(value.substring(0, index)), value.substring(index)];
+}
+
+const waitForElements = (elements) => {
+    return new Promise((resolve, reject) => {
+        const intervalID = setInterval(() => {
+            if (document.querySelector(elements)) {
+                clearInterval(intervalID);
+                resolve();
+            }
+        }, 500);
+    })
+};
+
+waitForElements("#contents > .ytd-rich-grid-renderer > #content").then(() => {
+    setInterval(() => {
+        let elements = document.querySelectorAll("[id=dismissible]");
+        elements.forEach((element) => {
+            // Remove short form videos
+            let time = element.querySelector("#time-status > span")?.innerHTML.trim().split(":").map((str) => Number(str));
+            if (time && time[0] == 0) {
+                element.parentNode.parentNode.parentNode.remove();
+            }
+
+            // Remove low view count videos
+            let views = element.querySelector("#metadata > #metadata-line > span")?.innerHTML.split(" ");
+            if (views) {
+                let [value, measure] = splitAtIndex(views[0], views[0].length - 1);
+                if (!["K", "M", "B"].includes(measure)) {
+                    element.parentNode.parentNode.parentNode.remove();
+                }
+            }
+
+            // Remove already watched videos
+            let watchedProgress = element.querySelector("#overlays > ytd-thumbnail-overlay-resume-playback-renderer > #progress")?.getAttribute("style");
+            if (watchedProgress) {
+                element.parentNode.parentNode.parentNode.remove();
+            }
+        })
+    }, 500);
+});
---
+++ src/filterWatching.js
@@ -0,0 +1,42 @@
+function splitAtIndex(value, index) {
+    return [Number(value.substring(0, index)), value.substring(index)];
+}
+
+const waitForElements = (elements) => {
+    return new Promise((resolve, reject) => {
+        const intervalID = setInterval(() => {
+            if (document.querySelector(elements)) {
+                clearInterval(intervalID);
+                resolve();
+            }
+        }, 500);
+    })
+};
+
+waitForElements("#contents > ytd-compact-video-renderer > #dismissible").then(() => {
+    setInterval(() => {
+        let elements = document.querySelectorAll("[id=dismissible]");
+        elements.forEach((element) => {
+            // Remove short form videos
+            let time = element.querySelector("#time-status > span")?.innerHTML.trim().split(":").map((str) => Number(str));
+            if (time && time[0] == 0) {
+                element.parentNode.remove();
+            }
+
+            // Remove low view count videos
+            let views = element.querySelector("#metadata > #metadata-line > span")?.innerHTML.split(" ");
+            if (views) {
+                let [value, measure] = splitAtIndex(views[0], views[0].length - 1);
+                if (!["K", "M", "B"].includes(measure)) {
+                    element.parentNode.remove();
+                }
+            }
+
+            // Remove already watched videos
+            let watchedProgress = element.querySelector("#overlays > ytd-thumbnail-overlay-resume-playback-renderer > #progress")?.getAttribute("style");
+            if (watchedProgress) {
+                element.parentNode.remove();
+            }
+        })
+    }, 500);
+});
